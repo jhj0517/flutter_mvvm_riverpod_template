e78f9ca2ad2c69099f16bc3ab38108fe5633f07b
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-09 22:58:49 +0900

    remove wrong package

--- __brick__/{{project_name.snakeCase()}}/lib/main.dart
+++ __brick__/{{project_name.snakeCase()}}/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mvvm_riverpod_mason_tmp/presentation/providers/theme/custom_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
