0dbf69d9a69fb040bb87401cc8b75c4cfd8a963a
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-09 22:44:59 +0900

    migrate provider to riverpod

--- __brick__/{{project_name.snakeCase()}}/lib/main.dart
+++ __brick__/{{project_name.snakeCase()}}/lib/main.dart
@@ -1,57 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_mvvm_riverpod_mason_tmp/presentation/providers/theme/custom_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'generated/l10n.dart';
 import 'presentation/views/views.dart';
 import 'presentation/providers/providers.dart';
-import 'data/repositories/repositories.dart';
 import 'di/dependency_injection.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DependencyInjection.register();
-  runApp(const MyApp());
+  runApp(
+      const ProviderScope(
+        child: MyApp()
+      )
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider<ThemeProvider>(
-              create: (context) => ThemeProvider(
-                  prefs: locator.get<SharedPreferences>()
-              )
-          ),
-          ChangeNotifierProvider<HomeProvider>(
-              create: (context) => HomeProvider(
-                memoRepository: locator.get<MemoRepository>(),
-                binanceRepository: locator.get<BinanceRepository>()
-              )
-          ),
-        ],
-        child: Consumer<ThemeProvider>(
-          builder: (context, themeProvider, child) {
-            return MaterialApp(
-              title: 'Flutter MVVM Provider Template',
-              localizationsDelegates: const [
-                S.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-              ],
-              supportedLocales: S.delegate.supportedLocales,
-              theme: themeProvider.attrs.colors,
-              home: const MyHomePage(),
-            );
-          },
-        )
+  Widget build(BuildContext context, WidgetRef ref) {
+    final customTheme = ref.watch(customThemeProvider);
+    return MaterialApp(
+      title: 'Flutter MVVM Provider Template',
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      theme: customTheme.colors,
+      home: const MyHomePage(),
     );
   }
 }
